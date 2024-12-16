44edbb19be1d47cd017f7719e57e417ba200618b
minhh4j <mnhh4jjjj@gmail.com>
2024-12-16 11:41:39 +0530

    Q9

---
+++ Q9.js
@@ -0,0 +1,17 @@
+var numberOfSteps = function(num) {
+    let steps = 0; 
+
+    while (num > 0) {
+        if (num % 2 === 0) {
+          
+            num = num / 2;
+        } else {
+           
+            num = num - 1;
+        }
+        steps++; 
+    } 
+    return steps; 
+}
+
+console.log(numberOfSteps(14))
