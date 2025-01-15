cfe2ca7ee27d8e89c94dbb2862bbccc9401bd05d
minhh4j <mnhh4jjjj@gmail.com>
2025-01-15 08:40:56 +0530

    Q28

--- Q27.js
+++ Q27.js
@@ -1,4 +1,3 @@
-
 function numIdenticalPairs(nums) {
     const count = {}
     let goodPairs = 0
@@ -6,7 +5,7 @@ function numIdenticalPairs(nums) {
     for (let i = 0; i < nums.length; i++) {
         const num = nums[i];
         if (count[num] === undefined) {
-            count[num] = 0;
+            count[num] = 0    
         }
         goodPairs += count[num]
         count[num]++
