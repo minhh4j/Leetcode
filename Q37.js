0654f56bf97fe2c8db77cd25dbe6250fe5ccfccc
minhh4j <mnhh4jjjj@gmail.com>
2025-03-04 10:48:46 +0530

    Q37

---
+++ Q37.js
@@ -0,0 +1,11 @@
+var minTimeToVisitAllPoints = function(points) {
+    let totalTime = 0 ;
+
+for(let i = 0 ; i < points.length - 1 ; i++){
+    let [x1 , y1 ] = points[i]
+    let [x2 , y2 ] = points[i + 1]
+    totalTime += Math.max(Math.abs(x1-x2)  , Math.abs(y1-y2))
+}
+return totalTime 
+
+};
